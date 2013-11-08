977025ca6c644188048a82cc281e868fd687a2de
Kristian Bjørke <kbjorke@gmail.com>
2013-11-08 12:15:44 +0100

    Made header file for the tridiagonal matrix solver.

---
+++ solve_tridiagonal_matrix_eq.h
@@ -0,0 +1,8 @@
+#ifndef SOLVE_TRIDIAGONAL_MATRIX_EQ_H
+#define SOLVE_TRIDIAGONAL_MATRIX_EQ_H
+
+
+void solve_tridiagonal_matrix_eq(int n, double *d, double *e,
+                                          double *x, double *b);
+
+#endif // SOLVE_TRIDIAGONAL_MATRIX_EQ_H
