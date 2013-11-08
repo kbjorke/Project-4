4a7daefe974f86a55d1a74434713a18b4c597ba6
Kristian Bjørke <kbjorke@gmail.com>
2013-11-08 14:31:39 +0100

    Header and source file for output function.

---
+++ output_data.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+void output(int n, int m, double T, double d,
+            double delta_t, double delta_x, double alpha,
+            char *method, double **u)
+{
+    int i, j;
+
+    string output_file;
+    ostringstream oss;
+
+    oss << "data_" << method  << "_n" <<
+           n << "_T" << int(T) << "_alpha" <<
+           setw(5) << setfill('0') << int(alpha*10000) <<
+           ".txt" << '\0' << endl;
+
+    output_file = oss.str();
+
+    fstream myfile;
+    myfile.open(output_file.c_str(), ios::out);
+
+    myfile << "T: " << T << '\t' <<
+              "d: " << d << '\t' <<
+              "dt: " << delta_t << '\t' <<
+              "dx: " << delta_x << '\t' <<
+              "n: " << n << '\t' <<
+              "m: " << m << '\t' <<
+              "alpha: " << alpha << '\t' <<
+              "method: " << method << '\t' << endl;
+
+    myfile.precision(10);
+    myfile << scientific;
+    for( j = 0; j < m; j++ ){
+        for( i = 0; i < n; i++ )
+        {
+            myfile << u[i][j] << "   ";
+        }
+        myfile << endl;
+    }
+
+    myfile.close();
+}
---
+++ output_data.h
@@ -0,0 +1,8 @@
+#ifndef OUTPUT_DATA_H
+#define OUTPUT_DATA_H
+
+void output(int n, int m, double T, double d,
+            double delta_t, double delta_x, double alpha,
+            char *method, double **u);
+
+#endif // OUTPUT_DATA_H
