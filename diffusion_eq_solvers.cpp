3f0903e05af56ab3e7517296a86f7dc58b23face
Kristian Bjørke <kbjorke@gmail.com>
2013-11-08 16:05:12 +0100

    Fixed some problems with backward euler and crank-nicolson.

--- diffusion_eq_solvers.cpp
+++ diffusion_eq_solvers.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "solve_tridiagonal_matrix_eq.h"
 
 
@@ -7,12 +5,11 @@ void forward_euler(int n, int m, double alpha, double **u)
 {
     static int i, j;
 
-    std::cout << alpha;
     for( j = 1; j < m; j++ ){
         for( i = 1; i < n-1; i++ )
         {
             u[i][j] = alpha*u[i-1][j-1] + ( 1 - 2*alpha )*u[i][j-1] +
-                    u[i+1][j-1];
+                    alpha*u[i+1][j-1];
         }
     }
 }
@@ -45,8 +42,9 @@ void backward_euler(int n, int m, double alpha, double **u)
         {
             V_prev[i-1] = u[i][j-1];
         }
+
         V_prev[0] += alpha*u[0][j-1];
-        V_prev[n-2] += alpha*u[n-1][j-1];
+        V_prev[n-3] += alpha*u[n-1][j-1];
 
         solve_tridiagonal_matrix_eq(n-2, A_d, A_e, V, V_prev);
 
@@ -82,14 +80,14 @@ void crank_nicolson(int n, int m, double alpha, double **u)
         for( i = 1; i < n-1; i++ )
         {
             V_prev[i-1] = alpha*u[i-1][j-1] + ( 2 - 2*alpha )*u[i][j-1] +
-                        u[i+1][j-1];
+                        alpha*u[i+1][j-1];
         }
         V_prev[0] += alpha*u[0][j-1];
-        V_prev[n-2] += alpha*u[n-1][j-1];
+        V_prev[n-3] += alpha*u[n-1][j-1];
 
         solve_tridiagonal_matrix_eq(n-2, A_d, A_e, V, V_prev);
 
-        for( i = 1; i < n-2; i++ )
+        for( i = 1; i < n-1; i++ )
         {
             u[i][j] = V[i-1];
         }
